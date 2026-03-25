2e113e3f22ca35b3ff8b35b495c488985ff22496
hyer7823 <hyer7823@gmail.com>
2026-03-25 14:38:32 +0900

    incording

---
+++ week3/quartiles.cpp
@@ -0,0 +1,52 @@
+﻿// quartiles.cpp
+/* 
+* 25% => 출력
+* 50% (중앙값) => 출력
+* 75% => 출력
+*/
+#include <iostream>
+#include <vector>
+#include <algorithm>  //sort()
+
+using namespace std;
+
+typedef vector<int>::size_type vec_sz;
+
+int quartile_calc(vector<int> data, int quartile) {
+	int size = data.size();
+	vec_sz q = size * (quartile / 100);      
+	
+	double median;
+	median = size % 2 == 0 ?    // if 조건
+		(homework[mid_pt] + homework[mid_pt - 1]) / 2 :  // if 참
+		homework[mid_pt];   // else 거짓
+
+	// 75%
+}
+
+void print_quartiles(vector<int> data) {
+           // 데이터를 오름차순으로 정렬
+	sort(data.begin(), data.end());
+
+	int q1 = quartile_calc(data, 25);     // 제1사분위수(25%)
+	int q2 = quartile_calc(data, 50);     // 제2사분위수(50%)
+	int q3 = quartile_calc(data, 75);     // 제3사분위수(75%)
+
+	cout <<
+		"Q1: " << q1 << endl <<
+		"Q2 (중앙값): " << q2 << endl <<
+		"Q3: " << q3 << endl;
+}
+
+int main(void) {
+
+	// 과제 점수의 중앙값을 구함
+	vec_sz mid_pt = size / 2;
+	double median;
+	median = size % 2 == 0 ?    // if 조건
+		(homework[mid_pt] + homework[mid_pt - 1]) / 2 :  // if 참
+		homework[mid_pt];   // else 거짓
+
+
+	return 0;
+}
